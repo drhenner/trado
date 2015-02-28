33062bf4e62754d6d3f0a0bf342a09722c455577
Tom Dallimore <me@tomdallimore.com>
2015-02-28 18:23:21 +0700

    Updated time for scheduled job. GR-14

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,30 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
 set :output, "/home/gimsonrobotics/current/log/schedule.log"
+
 job_type :rbenv_rake, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
                          cd :path && RAILS_ENV=production bundle exec rake :task --silent :output }
 job_type :rbenv_runner, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
                          cd :path && RAILS_ENV=production bundle exec rails runner :task --silent :output }
 
-every 1.day, :at => '4:10am' do
+every 1.day, :at => '4:00am' do
     rbenv_runner "Cart.clear_carts"
 end
 
