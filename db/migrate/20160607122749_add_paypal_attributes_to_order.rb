9d4dc2dc8698f6627b085d892ef68f605a5db34f
Tom Dallimore <me@tomdallimore.com>
2016-06-07 19:54:25 +0700

    Removed file due to conflict with production.

--- db/migrate/20160607122749_add_paypal_attributes_to_order.rb
+++
@@ -1,6 +0,0 @@
-class AddPaypalAttributesToOrder < ActiveRecord::Migration
-  def change
-    add_column :orders, :express_token, :string
-    add_column :orders, :express_payer_id, :string
-  end
-end
