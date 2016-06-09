662582af4bcf73a9bd9df025e41ae800f0919e05
Tom Dallimore <me@tomdallimore.com>
2016-06-09 17:59:06 +0700

    Fixed edit order button.

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -12,4 +12,8 @@ module OrderHelper
     def selected_country cart, order_address
         order_address.nil? ? cart.estimate_country_name : order_address
     end
+
+    def check_payment_type form_value, session
+        form_value == session ? true : false
+    end
 end
