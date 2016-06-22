6806f161b769af4c2ce292b7c27401337768d2c8
Tom Dallimore <me@tomdallimore.com>
2016-06-22 15:37:54 +0700

    Added company back in to email templates and added service notification email.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -11,4 +11,16 @@ class AdminMailer < ActionMailer::Base
             format.text { render "themes/#{Store.settings.theme.name}/emails/admin/order_notification", layout: "../themes/#{Store.settings.theme.name}/layout/email" }
         end
     end
+
+    def service_notification email, content
+        @content = content
+
+        mail(to: email, 
+            from: "#{Store.settings.name} <#{Store.settings.email}>",
+            subject: "Gimson Robotics Service Notification"
+        ) do |format|
+            format.html { render "themes/#{Store.settings.theme.name}/emails/admin/service_notification", layout: "../themes/#{Store.settings.theme.name}/layout/email" }
+            format.text { render "themes/#{Store.settings.theme.name}/emails/admin/service_notification", layout: "../themes/#{Store.settings.theme.name}/layout/email" }
+        end
+    end
 end
--- app/mailers/previews/admin_mailer_preview.rb
+++ app/mailers/previews/admin_mailer_preview.rb
@@ -4,6 +4,10 @@ class AdminMailerPreview < ActionMailer::Preview
         AdminMailer.order_notification(mock_order)
     end
 
+    def service_notification
+        AdminMailer.service_notification('admin@example.com', 'Hey this is a service notification!')
+    end
+
     private
 
     def mock_order
