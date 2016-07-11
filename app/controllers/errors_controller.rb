4973b674e06a8e6fc7a6e8a5005ce0c58c4b0b26
Tom Dallimore <me@tomdallimore.com>
2016-07-11 14:59:07 +0700

    Added featured products to 404.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,13 +2,17 @@ class ErrorsController < ApplicationController
     skip_before_action :authenticate_user!
     
     def show
+        set_featured_products if status_code.to_s == '404'
         render theme_presenter.page_template_path("errors/#{status_code.to_s}"), format: [:html], layout: theme_presenter.layout_template_path, status: status_code
     end
 
     protected
 
-        def status_code
-            params[:code] || 500
-        end
+    def status_code
+        params[:code] || 500
+    end
 
+    def set_featured_products
+        @featured_products = Product.active.published.featured.first(6)
+    end
 end
