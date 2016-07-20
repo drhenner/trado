d8d60173f9bf18c06d3aad02b33e41f9474676e6
Tom Dallimore <me@tomdallimore.com>
2016-07-20 11:22:56 +0700

    Enabled ssl.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, "gimsonrobotics"
 # This is used in the Nginx VirtualHost to specify which domains
 # the app should appear on. If you don't yet have DNS setup, you'll
 # need to create entries in your local Hosts file for testing.
-set :server_name, "54.194.71.162"
+set :server_name, "gimsonrobotics.co.uk"
 
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
@@ -25,4 +25,4 @@ set :unicorn_worker_count, 3
 
 # whether we're using ssl or not, used for building nginx
 # config file
-set :enable_ssl, false
+set :enable_ssl, true
