322cca41a2a0648d7f2719ebc8bd4c0ece55939c
Tom Dallimore <me@tomdallimore.com>
2016-08-04 22:14:24 +0700

    Fixing scheduler configs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,7 @@
-set :output, "/home/rails/current/log/schedule.log"
+set :output, "/home/deploy/apps/gimson_robotics_production/current/log/schedule.log"
 
-job_type :rbenv_rake, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
-                         cd :path && RAILS_ENV=production bundle exec rake :task --silent :output }
-job_type :rbenv_runner, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
-                         cd :path && RAILS_ENV=production bundle exec rails runner :task --silent :output }
+job_type :rbenv_rake, %Q{cd :path && RAILS_ENV=production RBENV_ROOT=/home/deploy/.rbenv RBENV_VERSION=2.3.0 /home/deploy/.rbenv/bin/rbenv exec bundle exec rake :task --silent :output }
+job_type :rbenv_runner, %Q{cd :path && RAILS_ENV=production RBENV_ROOT=/home/deploy/.rbenv RBENV_VERSION=2.3.0 /home/deploy/.rbenv/bin/rbenv exec bundle exec rails runner :task --silent :output }
 
 every 1.day, at: '4:00am' do
     rbenv_runner "Cart.clear_carts"
@@ -24,4 +22,3 @@ end
 every 1.hour do
     rbenv_runner "SendDispatchedOrderEmailsJob.perform_later"
 end
-
